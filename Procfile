e34c16ad6689f0f57a4533dc20457e3e3776e4c0
ainebye <ainebyemartin0@gmail.com>
2026-04-22 13:53:28 +0300

    22/04/2026, new update for hosting at render today

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bloodlink_project.wsgi --log-file -
+web: gunicorn app:application
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Add the project directory to Python path
+# Add to project directory to Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Set Django settings module
